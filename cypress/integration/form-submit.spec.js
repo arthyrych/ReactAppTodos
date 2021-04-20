d556516b14c682e65859f5ae2d88112b1f271715
Artem <arthyrych@gmail.com>
2021-04-20 20:34:05 +0300

    new test with form submission added

--- cypress/integration/form-submit.spec.js
+++ cypress/integration/form-submit.spec.js
@@ -8,4 +8,20 @@ describe('form submission test suite', () => {
         cy.wait('@save')
         cy.contains('new todo from fixtures').should('be.visible')
     })
+
+    it('shows an error message for a failed form submission', () => {
+        const newTodo = 'new todo from test const'
+        cy.server()
+        cy.route({
+            method: 'POST',
+            url: '/api/todos',
+            status: 500,
+            response: {}
+        }).as('save')
+        cy.seedAndVisit()
+        cy.get('.new-todo').type(newTodo).type('{enter}')
+        cy.wait('@save')
+        cy.get('.todo-list li').should('have.length', 4)
+        cy.get('.error').should('be.visible')
+    })
 })
